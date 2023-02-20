0aca2b7d7f0d0e627f01cafecdbfbed98f59bd1a
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-02-20 23:39:03 +0300

    CategoryRepository interface'i oluşturuldu

--- src/main/java/com/demo/ecommerce/entities/concretes/Category.java
+++ src/main/java/com/demo/ecommerce/entities/concretes/Category.java
@@ -1,4 +1,23 @@
 package com.demo.ecommerce.entities.concretes;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
 }
