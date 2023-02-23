ef0e077cc55665b4cde435dd01526788791612a5
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-02-23 22:10:31 +0300

    Product classı oluşturuldu

--- src/main/java/com/demo/ecommerce/entities/concretes/Category.java
+++ src/main/java/com/demo/ecommerce/entities/concretes/Category.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,8 +22,6 @@ public class Category {
     @Column(name = "name")
     private String name;
 
-    /*
-    oneToMany
-     */
-
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
--- src/main/java/com/demo/ecommerce/entities/concretes/Product.java
+++ src/main/java/com/demo/ecommerce/entities/concretes/Product.java
@@ -1,4 +1,30 @@
 package com.demo.ecommerce.entities.concretes;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
 }
