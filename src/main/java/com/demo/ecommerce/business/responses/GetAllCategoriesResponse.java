b33a2eab7322b3cb4f8eb709cae597de64ed0c75
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-03-03 08:12:14 +0300

    Get All Categories Response updated

--- src/main/java/com/demo/ecommerce/business/responses/GetAllCategoriesResponse.java
+++ src/main/java/com/demo/ecommerce/business/responses/GetAllCategoriesResponse.java
@@ -1,8 +1,11 @@
 package com.demo.ecommerce.business.responses;
 
+import com.demo.ecommerce.entities.concretes.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -10,4 +13,5 @@ import lombok.NoArgsConstructor;
 public class GetAllCategoriesResponse {
     private int id;
     private String name;
+    private List<Product> products;
 }
